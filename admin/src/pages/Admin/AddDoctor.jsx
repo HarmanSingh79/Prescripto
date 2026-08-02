e668feff4882afb461f5fbb6549e204666b7e4f2
HarmanSingh79 <harmansinghnew1@gmail.com>
2026-08-02 19:31:05 +0530

    fix

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -132,7 +132,7 @@ const AddDoctor = () => {
   }
 
   return (
-    <form onSubmit={onSubmitHandler} className='m-5 w-full max-sm:mx-auto'>
+    <form onSubmit={onSubmitHandler} className='m-5 w-full max-sm:w-[90vw] max-sm:mx-auto'>
 
       <p className='mb-3 text-lg font-medium'>Add Doctor</p>
 
